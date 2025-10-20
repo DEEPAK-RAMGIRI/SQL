44bc805676d0042705dc141033ceef6d9ced7b5a
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-10-20 10:47:34 +0530

    Create college database and student table with queries
    This script creates a database named 'college' and a table 'student' with various queries to manipulate and retrieve data.

---
+++ sql/basic2.sql
@@ -0,0 +1,44 @@
+create database college;
+
+use college;
+
+create table student (
+	rollno int primary key,
+    name varchar(50),
+    marks int not null,
+    grade varchar(1),
+    city varchar(15)
+);
+
+insert into student (rollno,name,marks,grade,city) values(101,"anil",78,"C","pune"),(102,"bhumika",93, "A","mumbai"),(103,"Chetan",85,"B","Mumbai"),
+(104,"dhruv",96,"A","Delhi"),(105,"emanual",12,"F","Delhi"),(106,"farah",82,"B","Delhi");
+
+select * from student;
+
+select name,marks from student;
+
+select city from student;
+
+select distinct city from student;
+
+select * from student where marks >= 80;
+
+select * from student where city = "Mumbai";
+
+select * from student where marks between 80 and 90;
+
+select * from student where city in ("Delhi","Mumbai");
+
+select * from student where city not in ("Delhi","Mumbai");
+
+select * from student where marks > 85 limit 3;
+
+select * from student order by marks asc;
+
+select * from student order by grade asc limit 3;
+
+select max(marks) from student;
+
+select count(*) from student;
+
+select city,count(name) from student group by city; 
